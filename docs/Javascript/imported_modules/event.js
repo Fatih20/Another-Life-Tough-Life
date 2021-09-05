9dbd46f7ff492c201ee4b07aeb81493756294bb4
Fatih20 <fnri39@protonmail.com>
2021-09-05 07:43:44 +0700

    Always insert space between end_game_story_bit regardless of lazy mode

--- docs/Javascript/imported_modules/event.js
+++ docs/Javascript/imported_modules/event.js
@@ -78,10 +78,7 @@ export class Event {
             } 
             index_of_compatible_condition += 1;
         }
-
-        if(this.Lazy_Mode) {
-            end_game_story_bit = end_game_story_bit + " ";
-        }
+        end_game_story_bit = end_game_story_bit + " ";
 
         if (ignore_paragraph){
             return {story_bit : end_game_story_bit};
